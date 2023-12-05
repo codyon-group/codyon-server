af328a8d1b8a01e44ab9cb86b33161fda7fe64ab
hee <qufslarkgml@gmail.com>
2023-12-05 21:15:18 +0900

    feat: error-handling 추가 #5

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,23 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ErrorHandler, HttpExceptionFilter, setValidatorError } from './exception/error.exception';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
 
+  app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalPipes(
     new ValidationPipe({
+      exceptionFactory: (errs): ErrorHandler => {
+        // stopAtFirstError 옵션으로 인해 errs[0]에만 error 사항 존재
+        const err = errs[0];
+        const errOption = Object.keys(err.constraints)[0];
+        const { code, detailCode, msg } = setValidatorError(errOption, err.property);
+
+        return new ErrorHandler(code, detailCode, msg);
+      },
       whitelist: true, // 정의된 속성 외 제외
       forbidNonWhitelisted: true, // 정의되지 않은 속성 에러 발생
       stopAtFirstError: true, // 첫 번째 에러 사항만 취급
