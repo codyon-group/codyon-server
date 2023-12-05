a48eb6b34595ce9bf04477a5c9480a345543a9a7
hee <qufslarkgml@gmail.com>
2023-12-05 20:04:30 +0900

    feat: db module 및 validationPipe 추가 #5

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // 정의된 속성 외 제외
+      forbidNonWhitelisted: true, // 정의되지 않은 속성 에러 발생
+      stopAtFirstError: true, // 첫 번째 에러 사항만 취급
+      transform: false, // 형 변환
+    }),
+  );
+
+  const configService = app.get(ConfigService);
+  const PORT = configService.get('PORT');
+
+  await app.listen(PORT, () => {
+    console.log(`Listening on Port: ${PORT}`);
+  });
 }
 bootstrap();
