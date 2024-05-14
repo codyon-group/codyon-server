bb25099f8f691c66f41dc0ce5b5e67a9226565d8
hee <qufslarkgml@gmail.com>
2024-05-14 17:59:12 +0900

    fix: 일반 & oauth 회원가입 시 좋아하는 스타일 태그 복호화 로직 추가 #21

--- src/common/common.service.ts
+++ src/common/common.service.ts
@@ -1,14 +1,21 @@
 import { Injectable } from '@nestjs/common';
-import { createCipheriv, createDecipheriv, randomBytes } from 'crypto';
-
-const ALGORITHM = 'aes-256-cbc';
-const KEY = randomBytes(32);
-const IV = randomBytes(16);
+import { ConfigService } from '@nestjs/config';
+import { createCipheriv, createDecipheriv } from 'crypto';
 
 @Injectable()
 export class CommonService {
+  private ALGORITHM: string;
+  private KEY: string;
+  private IV: string;
+
+  constructor(private configService: ConfigService) {
+    this.ALGORITHM = 'aes-256-cbc';
+    this.KEY = this.configService.get('ENCRYPTION_KEY');
+    this.IV = this.configService.get('ENCRYPTION_IV');
+  }
+
   encrypt(value: string): string {
-    const cipher = createCipheriv(ALGORITHM, KEY, IV);
+    const cipher = createCipheriv(this.ALGORITHM, this.KEY, this.IV);
 
     let encrypted = cipher.update(value, 'utf8', 'base64');
     encrypted += cipher.final('base64');
@@ -17,11 +24,19 @@ export class CommonService {
   }
 
   decrypt(encrypted: string): string {
-    const decipher = createDecipheriv(ALGORITHM, KEY, IV);
+    const decipher = createDecipheriv(this.ALGORITHM, this.KEY, this.IV);
 
     let decrypted = decipher.update(encrypted, 'base64', 'utf8');
     decrypted += decipher.final('utf8');
 
     return decrypted;
+  }
+
+  decryptList(encryptedList: string[]): string[] {
+    encryptedList.forEach((x, idx) => {
+      encryptedList[idx] = this.decrypt(x);
+    });
+
+    return encryptedList;
   }
 }
